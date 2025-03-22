3a8e2f179ac7da7cb8d1e1c2c4cdda1ec0950262
KRISHNA <154213033+krishnabhai99@users.noreply.github.com>
2025-03-22 13:40:29 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,22 +3,22 @@ import os
 
 class Config:
 
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = int(os.environ.get("API_ID", "14050586"))
+    API_HASH = os.environ.get("API_HASH", "42a60d9c657b106370c79bb0a8ac560c")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")]
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Krishna:krishna@cluster0.ecime.mongodb.net/")
+    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "5446367898").split(" ")]
     MAX_PROCESSES_PER_USER = int(os.environ.get("MAX_PROCESSES_PER_USER", 2))
     MAX_TRIM_DURATION = int(os.environ.get("MAX_TRIM_DURATION", 600))
     TRACK_CHANNEL = int(os.environ.get("TRACK_CHANNEL", False))
     SLOW_SPEED_DELAY = int(os.environ.get("SLOW_SPEED_DELAY", 5))
-    HOST = os.environ.get("HOST", "")
+    HOST = os.environ.get("HOST", "5446367898")
     TIMEOUT = int(os.environ.get("TIMEOUT", 60 * 30))
     DEBUG = bool(os.environ.get("DEBUG"))
     WORKER_COUNT = int(os.environ.get("WORKER_COUNT", 20))
-    IAM_HEADER = os.environ.get("IAM_HEADER", "")
+    IAM_HEADER = os.environ.get("IAM_HEADER", "5446367898")
 
     COLORS = [
         "white",
